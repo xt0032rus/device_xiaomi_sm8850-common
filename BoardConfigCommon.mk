895c6d2a8cf09dc0f3dde3bc6f58f124edf55c13
xt0032rus <andrey.denikin9@mail.com>
2026-03-24 03:49:48 +0300

    sm8850-common: import xiaomi specific manifest from stock
    Signed-off-by: xt0032rus <andrey.denikin9@mail.ru>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,8 +113,12 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.xiaomi.xml
+
 DEVICE_MANIFEST_SKUS := canoe
 DEVICE_MANIFEST_CANOE_FILES := \
+    $(COMMON_PATH)/configs/vintf/manifest_xiaomi.xml \
     hardware/qcom-caf/sm8850/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
     hardware/qcom-caf/sm8850/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
 
