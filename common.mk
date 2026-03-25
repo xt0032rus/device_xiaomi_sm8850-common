7e59115f3d87afb4d7ae56b558d3f22a3a07e995
xt0032rus <andrey.denikin9@mail.com>
2026-03-25 03:35:51 +0300

    sm8850-common: partially build agm from source
    Co-authored-by: lolipuru <banstellazz@gmail.com>
    Signed-off-by: xt0032rus <andrey.denikin9@mail.ru>

--- common.mk
+++ common.mk
@@ -66,6 +66,10 @@ PRODUCT_PACKAGES += \
     libsoundtriggerhal.qti
 
 PRODUCT_PACKAGES += \
+    libagm_compress_plugin \
+    libagm_mixer_plugin \
+    libagm_pcm_plugin \
+    libagmclient \
     libaudiochargerlistener \
     libaudiopreprocessing \
     libaudioserviceexampleimpl \
