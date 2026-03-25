704b25b8dbc8a6725664a1b2ae24b17ead5c3922
xt0032rus <andrey.denikin9@mail.com>
2026-03-25 03:45:13 +0300

    sm8850-common: Swap to QTI USB init scripts
    Signed-off-by: xt0032rus <andrey.denikin9@mail.ru>

--- common.mk
+++ common.mk
@@ -345,6 +345,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
     android.hardware.usb.gadget-service.qti
 
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+    
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
