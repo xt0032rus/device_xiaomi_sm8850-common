5cc51926ea8d9d0ab251e9b1931c252068ad1260
xt0032rus <andrey.denikin9@mail.com>
2026-03-27 17:57:47 +0300

    sm8550-common: Enable QSPA
    Signed-off-by: xt0032rus <andrey.denikin9@mail.ru>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,6 +70,7 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
+    androidboot.vendor.qspa=true \
     androidboot.load_modules_parallel=true \
     androidboot.hypervisor.protected_vm.supported=0 \
     androidboot.usbcontroller=a600000.dwc3 \
