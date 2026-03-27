549e95d250afe0239bcfbd458803ba19c8aab450
xt0032rus <andrey.denikin9@mail.com>
2026-03-27 17:39:42 +0300

    sm8850-common: Setup UDFPS
    Co-authored-by: lolipuru <banstellazz@gmail.com>
    Co-authored-by: 33bca <fabian.leutenegger@bluewin.ch>
    
    Signed-off-by: xt0032rus <andrey.denikin9@mail.ru>

---
+++ udfps/UdfpsHandler.cpp
@@ -0,0 +1,81 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-FileCopyrightText: Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHandler.xiaomi_sm8850"
+
+#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
+#include <android-base/logging.h>
+#include <android-base/unique_fd.h>
+
+#include <fstream>
+
+#include "UdfpsHandler.h"
+
+#define COMMAND_FOD_PRESS_STATUS 1
+#define COMMAND_FOD_PRESS_X 2
+#define COMMAND_FOD_PRESS_Y 3
+#define PARAM_FOD_PRESSED 1
+#define PARAM_FOD_RELEASED 0
+
+using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
+
+namespace {
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+}  // anonymous namespace
+
+class SM8850UdfpsHandler : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t* device) {
+        mDevice = device;
+    }
+
+    void onFingerDown(uint32_t x, uint32_t y, float /*minor*/, float /*major*/) {
+        LOG(DEBUG) << __func__ << "x: " << x << ", y: " << y;
+        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_X, x);
+        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_Y, y);
+        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_PRESSED);
+    }
+
+    void onFingerUp() {
+        LOG(DEBUG) << __func__;
+        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_X, 0);
+        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_Y, 0);
+        mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_RELEASED);
+    }
+
+    void onAcquired(int32_t result, int32_t vendorCode) {
+        LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
+        if (static_cast<AcquiredInfo>(result) == AcquiredInfo::VENDOR && vendorCode == 201) {
+            onFingerUp();
+        }
+    }
+
+    void cancel() {
+        LOG(INFO) << __func__;
+    }
+
+  private:
+    fingerprint_device_t* mDevice;
+};
+
+static UdfpsHandler* create() {
+    return new SM8850UdfpsHandler();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+        .create = create,
+        .destroy = destroy,
+};
